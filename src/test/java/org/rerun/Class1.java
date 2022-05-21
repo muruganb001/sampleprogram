a7feaf2408298f7e964021e385b8e928a5a230b9
muruganb001 <muruganb001@gmail.com>
2022-05-20 23:54:35 -0700

    bala

--- src/test/java/org/rerun/Class1.java
+++ src/test/java/org/rerun/Class1.java
@@ -20,6 +20,7 @@ public class Class1 {
 	private void tc04() {
 	Assert.assertTrue(false);
 		System.out.println("123456789");
+		System.out.println("balamurugan");
 	}
 
 }
---
+++ src/test/java/org/rerun/Class4.java
@@ -0,0 +1,9 @@
+package org.rerun;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		System.out.println("heelo bala");
+		System.out.println("balamurugan");
+	}
+}
