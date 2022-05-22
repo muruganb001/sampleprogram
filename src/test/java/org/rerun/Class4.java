186cb4a7306b86c6c5f8099a54a94f1c736cfb50
muruganb001 <muruganb001@gmail.com>
2022-05-22 00:59:02 -0700

    rearrange

--- src/test/java/org/rerun/Class4.java
+++ src/test/java/org/rerun/Class4.java
@@ -1,9 +1,18 @@
 package org.rerun;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Class4 {
 
 	public static void main(String[] args) {
 		System.out.println("heelo bala");
 		System.out.println("balamurugan");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
 	}
 }
